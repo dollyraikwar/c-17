d204e8d9efc43401e09bef91f71d4d8e72a5cd0a
dollyraikwar <42029960+dollyraikwar@users.noreply.github.com>
2018-08-02 14:57:59 +0530

    Add files via upload

---
+++ sort.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+int n,sum,pair=0;
+cout<<"enter the no of elements in array";
+cin>>n;
+vector<int>a(n);
+cout<<"enter the sum";
+cin>>sum;
+cout<<"enter the elements in array";
+for(int i=0;i<a.size();i++)
+{
+cin>>a[i];
+}
+for(int i=0;i<n;i++)
+{
+	for(int j=i+1;i<n;j++)
+	{
+		
+		
+	   if(a[i]+a[j]==sum)
+    	
+	 	pair++;
+	    
+	    
+	}
+}
+	 
+cout<<pair;
+return 0;
+}
